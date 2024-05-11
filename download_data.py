d9a73e6cd5c472b6eca6ab5b74da6f6f2be0b9ee
JNewman-Cell <jackson_newman@ucsb.edu>
2024-05-10 18:13:11 -0700

    added requirements.txt for flaskApp

--- download_data.py
+++ download_data.py
@@ -1,17 +1,17 @@
 import yfinance as yf
 import datetime
 
-# msft = yf.Ticker("MSFT")
+msft = yf.Ticker("MSFT")
 
 # # get all stock info
-# print(msft.info)
+print(msft.info)
 
 # # get historical market data
 # hist = msft.history(period="5yr")
 
-end_date = datetime.datetime.now()
-start_date = end_date - datetime.timedelta(days=5*365)
+# end_date = datetime.datetime.now()
+# start_date = end_date - datetime.timedelta(days=5*365)
 
-stock_data = yf.download("MSFT", start=start_date, end=end_date, interval='1wk')
+# stock_data = yf.download("MSFT", start=start_date, end=end_date, interval='1wk')
 
-print(stock_data)
+# print(stock_data)
